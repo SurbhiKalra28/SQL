9a695c35f4e3bbba0f66d0a7bbfa9be46f362d7e
SurbhiKalra28 <113727095+SurbhiKalra28@users.noreply.github.com>
2024-08-12 14:50:24 -0400

    Update healthcare_database.sql
    Formatting

--- healthcare_database.sql
+++ healthcare_database.sql
@@ -1,49 +1,49 @@
--- Ticket 2: Basic SELECT and WHERE Clause
+-- Ticket 2: Basic SELECT AND WHERE Clause
 Use HealthcareSystemDB;
-Select * from Patients where Gender = 'Female'; 
-Select Status from Appointments;
-Select * from Doctors where Specialization = 'Internal Medicine';
-Select * from Patients where DateOfBirth > '1985-01-01';
-Select * from MedicalRecords where Diagnosis like '%Cold%';
+Select * FROM Patients WHERE Gender = 'Female'; 
+Select Status FROM Appointments;
+Select * FROM Doctors WHERE Specialization = 'Internal Medicine';
+Select * FROM Patients WHERE DateOfBirth > '1985-01-01';
+Select * FROM MedicalRecords WHERE Diagnosis LIKE '%Cold%';
 
 -- Ticket 3: ORDER BY Clause
-Select * from Patients Order By LastName;
-Select * from Doctors Order By Specialization desc;
-Select * from Appointments Order By AppointmentDate desc;
-Select * from Departments Order By DepartmentName;
-Select * from Billing Order By BalanceAmount desc;
+Select * FROM Patients Order By LastName;
+Select * FROM Doctors Order By Specialization DESC;
+Select * FROM Appointments Order By AppointmentDate DESC;
+Select * FROM Departments Order By DepartmentName;
+Select * FROM Billing Order By BalanceAmount DESC;
 
--- Ticket 4: GROUP BY and HAVING Clauses
-Select country,count(*) from Patients Group By Country;
-Select DoctorID , Count(*) from Appointments Group By DoctorID;
-Select PatientID,sum(TotalAmount) from Billing Group By PatientID;
-Select DoctorID , Count(*) As TotalAppointments from Appointments Group By DoctorID having TotalAppointments > 1;
+-- Ticket 4: GROUP BY AND HAVING Clauses
+Select country,count(*) FROM Patients Group By Country;
+Select DoctorID , Count(*) FROM Appointments Group By DoctorID;
+Select PatientID,sum(TotalAmount) FROM Billing Group By PatientID;
+Select DoctorID , Count(*) As TotalAppointments FROM Appointments Group By DoctorID HAVING TotalAppointments > 1;
 
 -- Ticket 5: LIKE Operator
-Select * from Patients where FirstName Like 'J%';
-Select * from Doctors where LastName Like '%son';
-Select * from Departments where DepartmentName Like '%y';
-Select * from MedicalRecords where Diagnosis like 'Hypertension';
-Select * from patients where Email like '%example.com';
+Select * FROM Patients WHERE FirstName LIKE 'J%';
+Select * FROM Doctors WHERE LastName LIKE '%son';
+Select * FROM Departments WHERE DepartmentName LIKE '%y';
+Select * FROM MedicalRecords WHERE Diagnosis LIKE 'Hypertension';
+Select * FROM patients WHERE Email LIKE '%example.com';
 
 -- Ticket 6: BETWEEN Operator
-Select * from Appointments where AppointmentDate Between '2023-07-01' And '2023-07-03';
-Select * from Patients where DateOfBirth Between '1980-01-01' and '1990-12-31';
-Select * from Billing where TotalAmount Between '100' and '500';
-Select * from Appointments where AppointmentDate Between  '2023-07-01' And  '2023-07-08';
+Select * FROM Appointments WHERE AppointmentDate Between '2023-07-01' AND '2023-07-03';
+Select * FROM Patients WHERE DateOfBirth Between '1980-01-01' AND '1990-12-31';
+Select * FROM Billing WHERE TotalAmount Between '100' AND '500';
+Select * FROM Appointments WHERE AppointmentDate Between  '2023-07-01' AND  '2023-07-08';
 
 -- Ticket 7: LIMIT Operator
-Select * from Patients Order By LastName LIMIT 5;
-Select * from Billing Order By TotalAmount desc LIMIT 3;
-Select * from Appointments Order By AppointmentDate LIMIT 10;
-Select * from Doctors Order By FirstName LIMIT 5;
-Select * from Billing Order By BalanceAmount desc LIMIT 5;
+Select * FROM Patients Order By LastName LIMIT 5;
+Select * FROM Billing Order By TotalAmount DESC LIMIT 3;
+Select * FROM Appointments Order By AppointmentDate LIMIT 10;
+Select * FROM Doctors Order By FirstName LIMIT 5;
+Select * FROM Billing Order By BalanceAmount DESC LIMIT 5;
 
 -- Ticket 8: Chaining SQL Clauses
-Select * from Patients where Gender = 'Female' And Country = 'USA' Order BY LastName LIMIT 3;
-Select DoctorID, Count(*) As TotalAppointments from Appointments group By DoctorID having
-TotalAppointments > 2 Order By TotalAppointments desc;
-Select * from Patients Where LastName like 'S%' And year(DateOfBirth)>1985 Order By DateofBirth ;
-Select PatientID, count(*)  As numOfAppointmnets, sum(TotalAmount) AS finalAmounts  from Billing   Group BY PatientID 
-having numOfAppointmnets > 2 and finalAmounts Between 200 And 1500;
+Select * FROM Patients WHERE Gender = 'Female' AND Country = 'USA' Order BY LastName LIMIT 3;
+Select DoctorID, Count(*) As TotalAppointments FROM Appointments group By DoctorID HAVING
+TotalAppointments > 2 Order By TotalAppointments DESC;
+Select * FROM Patients WHERE LastName LIKE 'S%' AND year(DateOfBirth)>1985 Order By DateofBirth ;
+Select PatientID, count(*)  As numOfAppointmnets, sum(TotalAmount) AS finalAmounts  FROM Billing   Group BY PatientID 
+HAVING numOfAppointmnets > 2 AND finalAmounts Between 200 AND 1500;
 
